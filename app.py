c2df123d4178a57e68dc9d721fd2020efa983962
omlexac-source <omlexac@gmail.com>
2026-04-22 13:56:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Configuración de la IA con la llave que pusiste en Render
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-model = genai.GenerativeModel('models/gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 # Instrucciones para que la IA sepa qué es Okama
 SYSTEM_INSTRUCTION = (
